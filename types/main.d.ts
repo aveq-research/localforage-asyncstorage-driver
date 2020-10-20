b193ed780084508eda5cb52efaea4578adcb8d05
Arafat Zahan <kuasha420@gmail.com>
2020-10-20 09:51:10 +0600

    Update main.d.ts
    Module declaration is not needed for bundled types.

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,5 +1,3 @@
-declare module "@aveq-research/localforage-asyncstorage-driver" {
-  export function driverWithoutSerialization(): LocalForageDriver;
-  export function driverWithSerialization(serializer: LocalForageSerializer): LocalForageDriver;
-  export function driverWithDefaultSerialization(): LocalForageDriver;
-}
+export function driverWithoutSerialization(): LocalForageDriver;
+export function driverWithSerialization(serializer: LocalForageSerializer): LocalForageDriver;
+export function driverWithDefaultSerialization(): LocalForageDriver;
